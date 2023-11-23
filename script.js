8fff2760cd036b2fcef2ec42980064a9768b4aff
aleriiu <aleriiiu@gmail.com>
2023-11-23 19:30:30 +0300

    added burger menu

--- script.js
+++ script.js
@@ -74,6 +74,8 @@ const headerH = document.querySelector('.header-fixed').clientHeight;
 
 const headerLogo = document.querySelector('.header-logo');
 const seachBtn = document.querySelector('.header__nav-item_btn-input');
+const nav = document.querySelector('.header__nav-item-list');
+const burger = document.querySelector('.menu-button-container');
 
 document.onscroll = function () {
 
@@ -83,15 +85,17 @@ document.onscroll = function () {
         header.classList.add('fixed');
         document.body.style.paddingTop = headerH + 'px';
         headerLogo.style.maxWidth = '150' + 'px';
-        // seachBtn.style.paddingRight = '22' + 'rem';
         seachBtn.classList.add('input-padding');
+        nav.style.display = 'none';
+        burger.style.display = 'none';
     }
 
     else {
         header.classList.remove('fixed');
         document.body.removeAttribute('style');
         headerLogo.removeAttribute('style');
-        // seachBtn.removeAttribute('style');
         seachBtn.classList.remove('input-padding');
+        nav.removeAttribute('style');
+        burger.removeAttribute('style');
     }
 } 
